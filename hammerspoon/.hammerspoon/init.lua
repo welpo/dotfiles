57b5a5cd5c98a6ae0a193232850be88dc5254f29
welpo <welpo@users.noreply.github.com>
2025-03-23 22:17:48 +0100

    Hammerspoon: load modules conditionally

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,19 +1,16 @@
 hs.window.animationDuration = 0
 cmdCtrl = {"cmd", "ctrl"}
+local hostname = hs.host.localizedName():lower()
+local isWorkComputer = hostname:find("oo") ~= nil
 
 require("modules/caffeine")
 require("modules/lowerCase")
 require("modules/upperCase")
-require("modules/openAdmin")
 require("modules/scrolling")
 require("modules/sentenceCase")
-require("modules/shortcuts")
+require("modules/shortcuts")  -- keyboard shortcuts.
 require("modules/titleCase")
 require("modules/window")
--- MODE Report Finder
-MODE_CACHE_FILE="/tmp/mode_reports_cache.json"
-local modeReportFinder = require("modules/mode/reportFinder")
-local modeCacheBuilder = require("modules/mode/cacheBuilder")
 
 -- Auto-reload config on changes.
 function reloadConfig(files)
@@ -41,10 +38,17 @@ end
 
 hs.hotkey.bind({"cmd", "alt"}, "V", typeFromClipboard)
 
-hs.hotkey.bind({"cmd", "shift"}, "M", modeReportFinder.showFinder)
--- Refresh MODE cache every 2 hours.
-local updateTimer = modeCacheBuilder.setupPeriodicUpdates(2)
--- Force cache rebuild.
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "M", function()
-    modeCacheBuilder.updateCache()
-end)
+if isWorkComputer then
+    require("modules/openAdmin")
+    -- MODE Report Finder.
+    MODE_CACHE_FILE="/tmp/mode_reports_cache.json"
+    local modeReportFinder = require("modules/mode/reportFinder")
+    local modeCacheBuilder = require("modules/mode/cacheBuilder")
+    hs.hotkey.bind({"cmd", "shift"}, "M", modeReportFinder.showFinder)
+    -- Refresh MODE cache every 2 hours.
+    local updateTimer = modeCacheBuilder.setupPeriodicUpdates(2)
+    hs.hotkey.bind({"cmd", "shift", "ctrl"}, "M", function()
+        modeCacheBuilder.updateCache()
+    end)
+    print("Loaded work-specific modules")
+end
